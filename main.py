21bea0b490703d2ce83da9158e86876fb5d92bd3
thefuturecreator <keith@ultimateservices.us>
2025-04-11 16:08:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from starlette.requests import Request
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)

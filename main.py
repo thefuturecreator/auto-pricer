238461b6ea347a886599869857557416b194eb08
thefuturecreator <keith@ultimateservices.us>
2025-04-11 16:01:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# main.py placeholder
+ from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def quote_form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
